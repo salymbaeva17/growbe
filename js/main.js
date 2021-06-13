0c289a9c228388fed497bb83e0648a2624e08a48
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-06-13 23:46:58 +0600

    add more new sections and styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        margin: 30,
+        loop: true,
+        center: true,
+        stagePadding: 15,
+    });
+});
